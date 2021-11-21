fe13557593e016128d4e75b7e77a8efaa1a982ef
hiroki6207 <hiroki6207@gmail.com>
2021-11-21 14:19:23 +0900

    スクリプト修正

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -10,7 +10,7 @@ public class Spawner : MonoBehaviour
     // Start is called before the first frame update
     private void Start()
     {
-        StartCoroutine(Spawner()); //Coroutineを開始
+        StartCoroutine(SpawnLoop()); //Coroutineを開始
     }
 
     /// <summary>
